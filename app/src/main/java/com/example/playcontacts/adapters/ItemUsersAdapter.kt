7fee1d8fa7da0c23466884dad967aae8c25d1b95
Carlos Oliveros Alpirez <carlos.alpi21@gmail.com>
2022-07-27 08:53:59 -0500

    Se agrego la lista de usuarios con una carga de imagenes y letras

--- app/src/main/java/com/example/playcontacts/adapters/ItemUsersAdapter.kt
+++ app/src/main/java/com/example/playcontacts/adapters/ItemUsersAdapter.kt
@@ -5,9 +5,12 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.playcontacts.R
+import com.example.playcontacts.helpers.FunctionsHelper
 import com.example.playcontacts.models.User
 import com.example.playcontacts.models.UsersResponseModel
 
@@ -26,8 +29,29 @@ class ItemUsersAdapter(
         if (users == null) return
         Log.i("pos", position.toString() + "")
         val user: User = users!![position]
+
+        holder.lyUser.setOnClickListener {  }
         holder.tvName.text = user.name
         holder.tvEmail.text = user.email
+        holder.tvLetter.visibility = View.GONE
+        holder.ivAvatar.visibility = View.GONE
+        if (position % 2 == 0) {
+            holder.ivAvatar.visibility = View.VISIBLE
+            FunctionsHelper.glideBuilder(
+                context,
+                "https://industrialkem.com.mx/wp-content/plugins/elementskit/widgets/yelp/assets/images/profile-placeholder.png",
+                holder.ivAvatar
+            )
+        } else {
+            var letter = ""
+            user.name.split(" ").forEachIndexed { index, value ->
+                if (index <= 1) {
+                    letter += value.first().uppercaseChar()
+                }
+            }
+            holder.tvLetter.text = letter
+            holder.tvLetter.visibility = View.VISIBLE
+        }
     }
 
     override fun getItemCount(): Int {
@@ -35,6 +59,9 @@ class ItemUsersAdapter(
     }
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val lyUser: LinearLayout = itemView.findViewById(R.id.ly_selectable)
+        val tvLetter: TextView = itemView.findViewById(R.id.tv_letters)
+        val ivAvatar: ImageView = itemView.findViewById(R.id.iv_avatar)
         val tvName: TextView = itemView.findViewById(R.id.tv_name)
         val tvEmail: TextView = itemView.findViewById(R.id.tv_email)
     }
--- app/src/main/java/com/example/playcontacts/helpers/FunctionsHelper.kt
+++ app/src/main/java/com/example/playcontacts/helpers/FunctionsHelper.kt
@@ -1,10 +1,20 @@
 package com.example.playcontacts.helpers
 
+import android.content.Context
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.request.transition.DrawableCrossFadeFactory
+import com.example.playcontacts.R
 import com.example.playcontacts.models.ErrorModel
 import com.google.gson.Gson
 import retrofit2.Response
 
 object FunctionsHelper {
+
+    private val factory = DrawableCrossFadeFactory.Builder().setCrossFadeEnabled(true).build()
 
     fun errorResponseToObject(response: Response<*>): ErrorModel {
         val gson = Gson()
@@ -12,5 +22,13 @@ object FunctionsHelper {
             response.errorBody()!!.charStream(),
             ErrorModel::class.java
         )
+    }
+
+    fun glideBuilder(context: Context, urlImagen: String, imageView: ImageView) {
+        Glide.with(context).load(urlImagen)
+            .thumbnail(Glide.with(context).load(R.drawable.loading_gif).fitCenter()).fitCenter()
+            .apply(RequestOptions().diskCacheStrategy(DiskCacheStrategy.ALL).centerCrop())
+            .transition(DrawableTransitionOptions.withCrossFade(factory))
+            .into(imageView)
     }
 }
